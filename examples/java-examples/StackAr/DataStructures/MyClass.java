b071112eacd58dbb5212f398fd7528df7a6605e0
Monil Narang <monil.narang@mindtickle.com>
2022-08-08 21:54:33 +0530

    my class to generalize x+y

---
+++ examples/java-examples/StackAr/DataStructures/MyClass.java
@@ -0,0 +1,25 @@
+package DataStructures;
+
+public class MyClass {
+
+      public int hereFun(int x, int y) {
+        int z=0;
+        if (x==1 && y==2) { z = 3; }
+        if (x==2 && y==3) { z = 5; }
+        if (x==3 && y==4) { z = 7; }
+        if (x==4 && y==5) { z = 9; }
+        if (x==5 && y==6) { z = 11; }
+        if (x==6 && y==7) { z = 13; }
+        return z;
+      }
+
+      public static void main(String[] args) {
+            MyClass m = new MyClass();
+            m.hereFun(1, 2);
+            m.hereFun(2, 3);
+            m.hereFun(3, 4);
+            m.hereFun(4, 5);
+            m.hereFun(5, 6);
+            m.hereFun(6, 7);
+        }
+}
