c6fabf37cc57d4769308ec8c768b4748818937ac
Monil Narang <monil.narang@mindtickle.com>
2022-08-08 22:13:38 +0530

    trial update -6

--- examples/java-examples/StackAr/DataStructures/MyClass.java
+++ examples/java-examples/StackAr/DataStructures/MyClass.java
@@ -32,7 +32,7 @@ public class MyClass {
                   return 10;
               }
           }
-          return 0;
+          return x+1;
       }
 
       public static void main(String[] args) {
