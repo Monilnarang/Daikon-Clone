94496e56b3c8ebbf902fe06f7a31d4d4f3d5d953
Monil Narang <monil.narang@mindtickle.com>
2022-08-08 22:03:23 +0530

    trial update 3

--- examples/java-examples/StackAr/DataStructures/MyClass.java
+++ examples/java-examples/StackAr/DataStructures/MyClass.java
@@ -4,22 +4,14 @@ public class MyClass {
 
       public int z;
       public int hereFun(int x, int y) {
-        if (x==1 && y==2) { z = 3; }
-        if (x==2 && y==3) { z = 5; }
-        if (x==3 && y==4) { z = 7; }
-        if (x==4 && y==5) { z = 9; }
-        if (x==5 && y==6) { z = 11; }
-        if (x==6 && y==7) { z = 13; }
+        z = x + y;
         return z;
       }
 
       public static void main(String[] args) {
             MyClass m = new MyClass();
-            m.hereFun(1, 2);
-            m.hereFun(2, 3);
-            m.hereFun(3, 4);
-            m.hereFun(4, 5);
-            m.hereFun(5, 6);
-            m.hereFun(6, 7);
+            for (int i = 1; i < 99 ; i++) {
+                m.hereFun(i, i+1);
+            }
         }
 }
